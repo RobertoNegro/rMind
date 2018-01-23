60b49a0d9a8004e134228c86227827da2939add9
Roberto Negro <roberto.negro@studenti.unitn.it>
2018-01-23 17:23:38 +0100

    Aggiunte animazione invio\ricezione messaggi

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,7 +2,15 @@ var hasToResetFullscreen = false;
 var scrollbar;
 
 function isThinking(value) {
+	var hasToGoBottom = false;
+	if (value && scrollbar.scrollHeight - scrollbar.scrollTop === scrollbar.clientHeight) {
+		hasToGoBottom = true;
+	}
+
 	$('#thinking_indicators').toggleClass('show', value);
+
+	if (hasToGoBottom)
+		scrollToBottom();
 }
 
 function openAside() {
@@ -27,14 +35,15 @@ function toggleFullscreen() {
 }
 
 function scrollToBottom() {
+	$(scrollbar).stop();
 	$(scrollbar).animate({
 		scrollTop: $(scrollbar).prop("scrollHeight")
 	}, 500);
 }
 
-function addAsidePanFunctionality(el) {	
+function addAsidePanFunctionality(el) {
 	//delete Hammer.defaults.cssProps.userSelect;
-	
+
 	var manager = new Hammer.Manager(el);
 	manager.add(new Hammer.Pan({
 		domEvents: true,
@@ -52,7 +61,7 @@ function addAsidePanFunctionality(el) {
 
 function refreshSendVoiceButtons() {
 	if (($('#message_input').val()).length == 0) {
-			$('#voice_button').toggleClass('show', true);
+		$('#voice_button').toggleClass('show', true);
 		$('#send_button').toggleClass('show', false);
 	} else {
 		$('#voice_button').toggleClass('show', false);
@@ -60,13 +69,58 @@ function refreshSendVoiceButtons() {
 	}
 }
 
-$(document).ready(function () {
-	$('img').on('dragstart', function(event) { event.preventDefault(); });
+function addMessage(text, type) {
+	var el = $('<div class="message '+type+'">' + text + '</div>').appendTo('#chat_messages');
 	
+	var autoHeight = el.height();
+	var curMinHeight = el.css('min-height');
+	var curPaddingTop = el.css('padding-top');
+	var curPaddingBottom = el.css('padding-bottom');
+	var curColor = el.css('color');
+	
+	el.css({
+		'height': '0px',
+		'min-height': '0px',
+		'padding-top': '0rem',
+		'padding-bottom': '0rem',
+		'opacity': '0',
+		'color': 'rgba(0, 0, 0, 0)'
+	}).animate({
+		'height': autoHeight,
+		'min-height': curMinHeight,
+		'padding-top': curPaddingTop,
+		'padding-bottom': curPaddingBottom,
+		'opacity': '1',
+		'color': curColor
+	}, 250, 'swing');
+	
+	scrollToBottom();
+}
+
+function addSendMessage(text) {
+	addMessage(text, 'sent');
+}
+
+function addRecvMessage(text) {
+	addMessage(text, 'recv');
+}
+
+function sendMessage() {
+	addSendMessage($('#message_input').val());
+	// TODO: DUMMY FUNC SEND MESSAGE
+	
+	//isThinking(true);	
+}
+
+$(document).ready(function () {
+	$('img').on('dragstart', function (event) {
+		event.preventDefault();
+	});
+
 	autosize($('textarea'));
 
 	new SimpleBar($('#aside_scroller')[0]);
-	
+
 	scrollbar = (new SimpleBar($('#chat_scroller')[0])).getScrollElement();
 	scrollbar.addEventListener('scroll', function (event) {
 		var element = event.target;
@@ -97,11 +151,21 @@ $(document).ready(function () {
 			screenfull.toggle();
 		}
 	});
-	
+
 	$('#message_input').keyup(function () {
-        refreshSendVoiceButtons();
-   });
+		refreshSendVoiceButtons();
+	});
 	refreshSendVoiceButtons();
-	
-	isThinking(true);
+
+
+	setTimeout(function () {
+		isThinking(true);
+	}, 0);
+	setTimeout(function () {
+		isThinking(false);
+	}, 1000);
+	setTimeout(function () {
+		addRecvMessage("Dynamically added message");
+	}, 1000);
+
 });
